0fef5f21099918b6300fe9ac2432d0d218d8020e
August <augustwiesner16@gmail.com>
2023-01-01 20:02:01 -0600

    initiating project with new template

---
+++ src/LoginPage.tsx
@@ -0,0 +1,24 @@
+import React from "react";
+
+const LoginPage = () => {
+  return (
+    <div className='intro'>
+        <div className='intro-line'>
+        
+            <input 
+            className='intro-input-name intro-input'
+            placeholder='Name:'
+            ></input>
+        
+        
+            <input 
+            className='intro-input-password intro-input'
+            placeholder='Password:'
+            ></input>
+        
+        </div>
+    </div>
+  )
+}
+
+export default LoginPage
